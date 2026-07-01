307929ee5614a61714ca9504c60f0ad5dcd5b763
patriot793 <kungur-off@yandex.ru>
2026-07-01 14:18:35 +0600

    fix compose plugin build issue

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -17,6 +17,7 @@ android {
     }
 
     buildTypes {
+
         debug {
             isMinifyEnabled = false
             isDebuggable = true
@@ -53,4 +54,33 @@ android {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
         }
     }
+}
+
+dependencies {
+
+    implementation("androidx.core:core-ktx:1.13.1")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.4")
+    implementation("androidx.activity:activity-compose:1.9.2")
+
+    implementation(platform("androidx.compose:compose-bom:2024.10.00"))
+    implementation("androidx.compose.ui:ui")
+    implementation("androidx.compose.material3:material3")
+    implementation("androidx.compose.ui:ui-tooling-preview")
+
+    debugImplementation("androidx.compose.ui:ui-tooling")
+
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.4")
+
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.8.1")
+
+    implementation("com.squareup.okhttp3:okhttp:4.12.0")
+
+    implementation("androidx.camera:camera-core:1.3.4")
+    implementation("androidx.camera:camera-camera2:1.3.4")
+    implementation("androidx.camera:camera-lifecycle:1.3.4")
+    implementation("androidx.camera:camera-view:1.3.4")
+
+    implementation("com.google.mlkit:barcode-scanning:17.3.0")
+
+    implementation("com.google.code.gson:gson:2.11.0")
 }
